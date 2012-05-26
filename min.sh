3feb822771b630345f6618dac51a4fcc9e8f484d
Philip Zeyliger <philip@cloudera.com>
2012-05-26 09:27:43 -0700

    min.sh

--- min.sh
+++ min.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # 
 # Minimal configuration that I prefer when using remote machines.
+#
+# Evaluate with:
+# . <(curl https://raw.github.com/philz/snippets/master/min.sh)
 
 echo Hello
 
